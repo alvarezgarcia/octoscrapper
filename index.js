642f1508f7c4dc577c1999784334ea9f11e8754d
Sebastián Alvarez <seba.alvarez16@gmail.com>
2016-12-28 19:41:47 -0300

    Agregadas funciones de descarga

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Immutable from 'immutable';
 import async from 'async';
 import requestModule from 'request'
 
-import { scrap } from './utils/';
+import { scrap, getPages, downloadHtml } from './utils/';
 
 const config = require('./config/config.json');
 
@@ -18,12 +18,18 @@ request.post({
 		op						: config.login.op
 	}}, (err, httpResponse, body) => {
 				if(err) throw err;
+				console.log('Conectado...');
 
-				async.parallel(	config.instrumentos.map((c) => scrap(c, request)),
-											(err, results) => {
-												console.log(results)
+				const instrumentosPagesTasks = config.instrumentos.map((c) => getPages(c, request));
+
+				async.parallel(instrumentosPagesTasks, (err, downloadData) => {
+					const downloadHtmlTasks = downloadData.map((dd) => downloadHtml(dd, request));
+
+					async.parallel(downloadHtmlTasks, (err, htmlData) => {
+						console.log(htmlData)
+					})
+
 				})
-	}
-)
+})
 
 
--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,132 @@
+import fs from 'fs'
+
+import cheerio from 'cheerio'
+import async from 'async'
+
+const fields = require('../config/fields.json')
+
+function downloadHtml(d, request) {
+
+	return (done) => {
+		const {nombreInstrumento, url, lastPage} = d;
+
+		const instrumentPagesTasks =  Array(lastPage + 1).join(".").split(".").map( (p, i) => {
+			return (done) => {
+				const pageUrl = `${url}&page=${i}`;
+				const fieldsList = fields[nombreInstrumento];
+
+				_download(request, pageUrl, (err, html) => {
+					_saveHtml(html, `/tmp/${nombreInstrumento}-${i}.html`, (err, fileName) => {
+						done(null, fileName);
+					});
+				});
+			}
+		});
+
+		async.parallel(instrumentPagesTasks, (err, data) => {
+			const o = {
+				nombreInstrumento: nombreInstrumento,
+				archivosInstrumento: data
+			}
+
+			console.log(nombreInstrumento, ' descargado');
+			done(null, o);
+		})
+
+	}
+
+}
+
+
+function getPages({nombreInstrumento, url}, request) {
+	return (done) => {
+
+		request.get(url, (err, httpResponse, body) => {
+			let $ = cheerio.load(body);
+
+			const pages = $('.pager-last').children();
+			const lastPage = pages.attr('href').split('&page=')[1]
+
+			done(null, {nombreInstrumento, url, lastPage: parseInt(lastPage)});
+		});
+
+	}
+}
+
+function _download(request, url, callback) {
+
+	request.get(url, (err, httpResponse, body) => {
+		callback(undefined, body);
+	});
+
+}
+
+function _saveHtml(htmlCode, destFile, callback) {
+
+	fs.writeFile(destFile, htmlCode, (err) => {
+		callback(undefined, destFile);
+	});
+
+}
+
+function __download(request, fieldsList, url, callback) {
+	request.get(url, (err, httpResponse, body) => {
+		let $ = cheerio.load(body);
+
+
+		let qa = fieldsList.map((f) => {
+			return $('.'+f).text();
+		});
+
+		let ff = qa.reduce((acc, r) => {
+			const p = r.split('\n');
+			const question = p.slice(1, 2);
+			const answers = p.slice(2, p.length);
+
+			acc[question] = [];
+			acc[question] = answers;
+
+			return acc;
+		}, {});
+
+		callback(undefined, ff);
+
+	});
+
+}
+
+
 function scrap({nombreInstrumento, url}, request) {
 
 	return (done) => {
 		request.get({url}, (err, httpResponse, body) => {
-			done(null, body)
+			let $ = cheerio.load(body);
+
+			const fieldsList = fields[nombreInstrumento];
+
+
+			let qa = fieldsList.map((f) => {
+				return $('.'+f).text();
+			});
+
+			let ff = qa.reduce((acc, r) => {
+				const p = r.split('\n');
+				const question = p.slice(1, 2);
+				const answers = p.slice(2, p.length);
+
+				acc[question] = [];
+				acc[question] = answers;
+
+				return acc;
+			}, {});
+
+
+			console.log(ff);
+
+			done(null, body);
 		})
 	}
 
 }
 
-export { scrap }
+export { scrap, getPages, downloadHtml }
