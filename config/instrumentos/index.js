89c3a21350aadc27b1b4d8db82db4adcaf4c5bc9
Sebastián Alvarez <seba.alvarez16@gmail.com>
2017-01-24 23:02:26 -0300

    Agregadas las clases html de consulta-joven

--- config/instrumentos/index.js
+++ config/instrumentos/index.js
@@ -1,7 +1,9 @@
 import centrosEducativos from './centros-educativos'
 import entrevistasAEmpresas from './entrevistas-a-empresas'
+import consultaJoven from './consulta-joven'
 
 export default {
 	'centros-educativos'		: centrosEducativos,
-	'entrevistas-a-empresas': entrevistasAEmpresas
+	'entrevistas-a-empresas': entrevistasAEmpresas,
+	'consulta-joven'				: consultaJoven
 }
