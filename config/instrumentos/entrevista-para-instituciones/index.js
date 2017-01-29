b0215f8f42dbb3be014ed54cff51527dea5e454c
Sebastián Alvarez <seba.alvarez16@gmail.com>
2017-01-29 18:30:49 -0300

    Agregado entrevista para instituciones

---
+++ config/instrumentos/entrevista-para-instituciones/index.js
@@ -0,0 +1,19 @@
+export default [
+	"views-field-field-1-fecha",
+	"views-field-field-2-pa-s",
+	"views-field-field-3-departamento",
+	"views-field-field-4-municipio",
+	"views-field-field-5-cefi",
+	"views-field-title",
+	"views-field-field-7-refer-de-contacto",
+	"views-field-field-8-data-de-contacto",
+	"views-field-field-9-tipo-de-instituci-n-fund",
+	"views-field-field-10-ubicaci-n-de-la-institu",
+	"views-field-field-11-su-organizaci-n-da-opor",
+	"views-field-field-12-en-qu-actividades-y-pro",
+	"views-field-field-16-cu-les-son-los-sectores",
+	"views-field-field-acl-sector-econ-mico",
+	"views-field-field-14-cu-les-son-los-oficios-",
+	"views-field-field-15-qu-actividades-realiza-",
+	"views-field-field-aclarar-actividad-"
+]
