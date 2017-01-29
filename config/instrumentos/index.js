9eaf1e087585f33dcf85776d30cabf3ef1541f55
Sebastián Alvarez <seba.alvarez16@gmail.com>
2017-01-29 17:47:34 -0300

    Agregado entrevista a asociaciones de caficultores

--- config/instrumentos/index.js
+++ config/instrumentos/index.js
@@ -1,9 +1,11 @@
+import consultaJoven from './consulta-joven'
 import centrosEducativos from './centros-educativos'
 import entrevistasAEmpresas from './entrevistas-a-empresas'
-import consultaJoven from './consulta-joven'
+import entrevistaAsocCaficultores from './entrevista-asoc-de-caficultores'
 
 export default {
-	'centros-educativos'		: centrosEducativos,
-	'entrevistas-a-empresas': entrevistasAEmpresas,
-	'consulta-joven'				: consultaJoven
+	'centros-educativos'							: centrosEducativos,
+	'entrevistas-a-empresas'					: entrevistasAEmpresas,
+	'consulta-joven'									: consultaJoven,
+	'entrevista-asoc-de-caficultores'	: entrevistaAsocCaficultores
 }
