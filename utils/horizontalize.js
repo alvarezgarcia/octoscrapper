f8100d03766e8f750e9a07f5c1134403403bb0fe
Sebastián Alvarez <seba.alvarez16@gmail.com>
2017-03-09 10:58:56 -0300

    Fixes

--- utils/horizontalize.js
+++ utils/horizontalize.js
@@ -3,7 +3,6 @@ import async from 'async';
 
 
 const collectionList = [ 
-/*
 	'abandono-de-jovenes',
 	'centros-educativos',
 	'consulta-joven',
@@ -13,9 +12,7 @@ const collectionList = [
 	'entrevista-para-instituciones',
 	'entrevistas-a-empresas',
 	'entrevistas-a-productores-y-familias',
-*/
 	'ficha-institucion-gaag',
-/*
 	'fichas-pfo',
 	'linea-de-base-formadores-territorio',
 	'linea-de-base-joven',
@@ -23,7 +20,6 @@ const collectionList = [
 	'rubrica-de-competencias-momento-0',
 	'rubrica-de-competencias-momento-1',
 	'rubrica-de-competencias-momento-2'
-*/
 ]
 
 
@@ -31,8 +27,18 @@ const integersFields = [
 	'14_ ¿Cuál es tu remuneración aproximada?',
 	'7_ Cantidad total de instituciones que conforman este GAAG',
 	'24_ Cantidad de convenios/acuerdos firmados para “formalizar” las oportunidades de formación y pasantía y empleo a jóvenes con',
+	'9_ Indicar cantidad de empresas del GAAG',
+	'10_ Indicar cantidad de asociaciones de productores del GAAG',
+  '11_ Indicar cantidad de centros educativos del GAAG', 
 	'12_ Indicar cantidad de familias del GAAG',
-		'10_ Indicar cantidad de asociaciones de productores del GAAG'
+  '13_ Indicar cantidad de otras instituciones del GAAG',
+	'14_1 Empleos formales',
+  '14_2 Actividad vinculada al café',
+  '14_3 Pasantías o prácticas a realizar en un ámbito laboral o cooperativa',
+  '14_4 Proyectos de micro-emprendimientos productivos',
+  '14_5 Cursos en instituciones de Formación Profesional',
+  '14_6 Reinserción educativa para quienes deban concluir sus estudios',
+  '14_7 Becas universitarias y apoyo en las condiciones de acceso a una carrera'
 ]
 
 MongoClient.connect('mongodb://localhost:27017/ses', (err, db) => {
@@ -79,6 +85,7 @@ MongoClient.connect('mongodb://localhost:27017/ses', (err, db) => {
 	})
 
 	async.series(tasks, (err, results) => {
+		console.log('Horiz terminada')
 		//console.log(results)
 	})
 
